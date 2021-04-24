309f20ac416848b54d97168b765a40c4546f0789
andim461 <andim461@yandex.ru>
2021-04-24 17:36:15 +0300

    some change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Main from "./components/Main";
 import { makeStyles } from "@material-ui/styles";
 const useStyles = makeStyles({
   app: {
-    background: "linear-gradient(50deg, #00FF00 10%, #FF8E53 90%)",
+    background: "linear-gradient(50deg, #06FF11 15%, #FF8E53 70%)",
     position: "absolute",
     height: "100%",
     width: "100%",
